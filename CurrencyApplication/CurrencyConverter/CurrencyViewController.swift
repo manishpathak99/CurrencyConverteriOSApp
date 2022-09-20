42705afd49f8f63b5d0e983f78e044cdbb0d74a7
Manish Pathak <manish.pathak@nagarro.com>
2022-09-20 15:22:54 +0530

    Implemented the UI in storyboard and connected the IBActions.

--- CurrencyApplication/CurrencyConverter/CurrencyViewController.swift
+++ CurrencyApplication/CurrencyConverter/CurrencyViewController.swift
@@ -9,5 +9,36 @@
 import UIKit
 
 class CurrencyViewController: UIViewController {
+    
+    // MARK: - IBOutlet
+    @IBOutlet private weak var fromCurrencyButton: CurrencyButton!
+    @IBOutlet private weak var toCurrencyButton: CurrencyButton!
+    @IBOutlet private weak var currencyTextfield: UITextField!
+    @IBOutlet private weak var convertedTextField: UITextField! {
+        didSet{
+            convertedTextField.isUserInteractionEnabled = false
+        }
+    }
+    fileprivate let loader = UIActivityIndicatorView(style: UIActivityIndicatorView.Style.large)
+    
+    // MARK: - IBOutlet Action
+    @IBAction func btnFromCurrencyClicked(_ sender: Any) {
+        fromCurrencyClicked()
+    }
+    
+    @IBAction func btnToCurrencyClicked(_ sender: Any) {
+        toCurrencyClicked()
+    }
+}
 
+extension CurrencyViewController {
+    
+    // MARK: - Fileprivate Functions
+    fileprivate func fromCurrencyClicked() {
+        
+    }
+    
+    fileprivate func toCurrencyClicked() {
+        
+    }
 }
