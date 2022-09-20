e41d486d4b0d8e7b97b95433a448797eb575dd85
Manish Pathak <manish.pathak@nagarro.com>
2022-09-20 13:16:06 +0530

    Added HTTP enum to Network Layer

---
+++ CurrencyApplication/Network/Request/HTTPMethod.swift
@@ -0,0 +1,15 @@
+//
+//  HTTPMethods.swift
+//  CurrencyApplication
+//
+//  Created by Manish Pathak on 20/09/22.
+//  Copyright © 2022 Manish Pathak. All rights reserved.
+//
+
+public enum HTTPMethod: String {
+    case get     = "GET"
+    case post    = "POST"
+    case put     = "PUT"
+    case patch   = "PATCH"
+    case delete  = "DELETE"
+}
