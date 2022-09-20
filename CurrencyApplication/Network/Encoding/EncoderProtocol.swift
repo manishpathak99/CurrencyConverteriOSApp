2cc58cb634ae2f250036bcd9a2d3b41669ef4873
Manish Pathak <manish.pathak@nagarro.com>
2022-09-20 13:29:00 +0530

    Added HTTP Parameter Encoding to Network Layer

---
+++ CurrencyApplication/Network/Encoding/EncoderProtocol.swift
@@ -0,0 +1,14 @@
+//
+//  EncoderProtocol.swift
+//  CurrencyApplication
+//
+//  Created by Manish Pathak on 20/09/22.
+//  Copyright © 2022 Manish Pathak. All rights reserved.
+//
+
+import Foundation
+
+public typealias Parameters = [String:Any]
+public protocol ParameterEncoder {
+    func encode(urlRequest: inout URLRequest, with parameters: Parameters) throws
+}
---
+++ CurrencyApplication/Network/Encoding/HTTPParameterEncoding.swift
@@ -0,0 +1,49 @@
+//
+//  ParameterEncoding.swift
+//  CurrencyApplication
+//
+//  Created by Manish Pathak on 20/09/22.
+//  Copyright © 2022 Manish Pathak. All rights reserved.
+//
+
+import Foundation
+
+
+public enum ParameterEncoding {
+    
+    case urlEncoding
+    case jsonEncoding
+    case urlAndJsonEncoding
+    
+    public func encode(urlRequest: inout URLRequest,
+                       bodyParameters: Parameters?,
+                       urlParameters: Parameters?) throws {
+        do {
+            switch self {
+            case .urlEncoding:
+                guard let urlParameters = urlParameters else { return }
+                try URLParameterEncoder().encode(urlRequest: &urlRequest, with: urlParameters)
+                
+            case .jsonEncoding:
+                guard let bodyParameters = bodyParameters else { return }
+                try JSONParameterEncoder().encode(urlRequest: &urlRequest, with: bodyParameters)
+                
+            case .urlAndJsonEncoding:
+                guard let bodyParameters = bodyParameters,
+                    let urlParameters = urlParameters else { return }
+                try URLParameterEncoder().encode(urlRequest: &urlRequest, with: urlParameters)
+                try JSONParameterEncoder().encode(urlRequest: &urlRequest, with: bodyParameters)
+                
+            }
+        }catch {
+            throw error
+        }
+    }
+}
+
+
+public enum NetworkError : String, Error {
+    case parametersNil = "Parameters were nil."
+    case encodingFailed = "Parameter encoding failed."
+    case missingURL = "URL is nil."
+}
